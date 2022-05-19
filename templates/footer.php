38d239dca4df3687190ca7204b604002413baecd
Tavilo Breno <breno_wk2@hotmail.com>
2022-05-19 00:37:28 -0300

    Estilizando o rodapé

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
  <footer>
-    <p>Pizzaria do João &copy; 2021</p>
+    <p>Pizzaria do João &copy; <?= date("Y") ?></p>
   </footer>
   <!-- Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
